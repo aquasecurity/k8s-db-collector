ac43cc87c1711cc2c5c18cfe287a7117809b239c
chenk <hen.keinan@gmail.com>
2022-07-21 11:32:11 +0300

    test: update model data (#24)
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- collectors/outdatedapi/outdated/outdated_test.go
+++ collectors/outdatedapi/outdated/outdated_test.go
@@ -15,13 +15,13 @@ func TestMapResources(t *testing.T) {
 		{name: "override removed version",
 			mdAPI:      []*OutdatedAPI{{Removed: "1.25", Gav: Gvk{Group: "storage.k8s.io", Version: "v1beta1", Kind: "CSIStorageCapacity"}}},
 			swaggerAPI: map[string]*OutdatedAPI{"io.k8s.api.storage.k8s.io.v1beta1.CSIStorageCapacity": {Removed: "1.23", Deprecated: "1.21", Gav: Gvk{Group: "storage.k8s.io", Version: "v1beta1", Kind: "CSIStorageCapacity"}}},
-			want:       []K8sAPI{{DeprecatedVersion: "1.21", RemovedVersion: "1.25", API: "storage.k8s.io.v1beta1.CSIStorageCapacity"}},
+			want:       []K8sAPI{{DeprecatedVersion: "1.21", RemovedVersion: "1.25", Version: "v1beta1", Group: "storage.k8s.io", Kind: "CSIStorageCapacity"}},
 		},
 		{name: "append api",
 			mdAPI:      []*OutdatedAPI{{Removed: "1.25", Gav: Gvk{Group: "flowcontrol.apiserver.k8s.io", Version: "v1beta1", Kind: "FlowSchema"}}},
 			swaggerAPI: map[string]*OutdatedAPI{"io.k8s.api.storage.k8s.io.v1beta1.CSIStorageCapacity": {Removed: "1.23", Deprecated: "1.21", Gav: Gvk{Group: "storage.k8s.io", Version: "v1beta1", Kind: "CSIStorageCapacity"}}},
-			want: []K8sAPI{{DeprecatedVersion: "", RemovedVersion: "1.25", API: "flowcontrol.apiserver.k8s.io.v1beta1.FlowSchema"},
-				{DeprecatedVersion: "1.21", RemovedVersion: "1.23", API: "storage.k8s.io.v1beta1.CSIStorageCapacity"}},
+			want: []K8sAPI{{DeprecatedVersion: "", RemovedVersion: "1.25", Version: "v1beta1", Group: "flowcontrol.apiserver.k8s.io", Kind: "FlowSchema"},
+				{DeprecatedVersion: "1.21", RemovedVersion: "1.23", Version: "v1beta1", Group: "storage.k8s.io", Kind: "CSIStorageCapacity"}},
 		},
 	}
 
@@ -29,7 +29,9 @@ func TestMapResources(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := MergeMdSwaggerVersions(tt.mdAPI, tt.swaggerAPI)
 			for index, r := range got {
-				assert.Equal(t, r.API, tt.want[index].API)
+				assert.Equal(t, r.Group, tt.want[index].Group)
+				assert.Equal(t, r.Version, tt.want[index].Version)
+				assert.Equal(t, r.Kind, tt.want[index].Kind)
 				assert.Equal(t, r.DeprecatedVersion, tt.want[index].DeprecatedVersion)
 				assert.Equal(t, r.RemovedVersion, tt.want[index].RemovedVersion)
 			}
