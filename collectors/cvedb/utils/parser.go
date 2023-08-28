0cb66f261d038e9af0c934ada740599a5eb294e3
chenk <hen.keinan@gmail.com>
2023-08-28 19:18:48 +0300

    test: add parser checks
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- collectors/cvedb/utils/parser.go
+++ collectors/cvedb/utils/parser.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"fmt"
 
-	"regexp"
 	"strings"
 
 	version "github.com/aquasecurity/go-pep440-version"
@@ -40,84 +39,6 @@ func TrimString(version string, trimValues []string) string {
 		version = strings.ReplaceAll(version, v, "")
 	}
 	return strings.TrimSpace(version)
-}
-
-func MatchRegEx(regex string, value string) bool {
-	headerRegex := regexp.MustCompile(regex)
-	return len(headerRegex.FindStringSubmatch(value)) > 0
-
-}
-
-func VersionParts(line string) ([]string, string) {
-	line = strings.TrimSpace(line)
-	line = strings.TrimPrefix(line, "-")
-	sign := "-"
-	signs := []string{"<=", ">=", "<", ">"}
-	for _, s := range signs {
-		if strings.Contains(line, s) {
-			sign = s
-			break
-		}
-	}
-	if sign != "-" {
-		line = strings.ReplaceAll(line, sign, "")
-	}
-	updatedLine := fmt.Sprintf(" %s", line)
-	findVersionParts := make([]string, 0)
-	versionRex := `(?P<name>[^\s]+)?\s+v?(?P<version>\d+\.\d+\.\d+).*.v?(?P<version2>\d+\.\d+\.\d+)|\s*(\d+\.\d+\.\d+)\s*|(?P<name2>[^\s]+)?\s+v?(?P<version3>\d+\.\d+\.\d+)`
-	regex := regexp.MustCompile(versionRex)
-	parts := regex.FindStringSubmatch(updatedLine)
-	if IsIP(updatedLine) {
-		return findVersionParts, ""
-	}
-	for i, p := range parts {
-		if i == 0 {
-			continue
-		}
-		if len(p) == 0 {
-			continue
-		}
-		findVersionParts = append(findVersionParts, p)
-		if len(findVersionParts) == 1 && sign == "-" {
-			sign = ""
-		}
-	}
-	return findVersionParts, sign
-}
-
-func IsIP(value string) bool {
-	ipregex := `(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])`
-	regex := regexp.MustCompile(ipregex)
-	parts := regex.FindStringSubmatch(value)
-	return len(parts) > 0
-}
-
-func UpdatedLine(versionParts []string, sign string) string {
-	switch len(versionParts) {
-	case 1:
-		return fmt.Sprintf("- %s v%s", sign, versionParts[0])
-	case 2:
-		var oneVersion bool
-		var twoVersion bool
-		if _, err := version.Parse(versionParts[0]); err == nil {
-			oneVersion = true
-		}
-		if _, err := version.Parse(versionParts[1]); err == nil && oneVersion {
-			twoVersion = true
-			oneVersion = false
-		} else {
-			oneVersion = true
-		}
-		if twoVersion {
-			return fmt.Sprintf("- v%s - v%s", versionParts[0], versionParts[1])
-		}
-		if oneVersion {
-			return fmt.Sprintf("- %s %s v%s", versionParts[0], sign, versionParts[1])
-		}
-	case 3:
-		return fmt.Sprintf("- %s v%s - v%s", versionParts[0], versionParts[1], versionParts[2])
-	}
-	return ""
 }
 
 func CvssVectorToScore(vector string) (string, float64) {
@@ -165,23 +86,6 @@ func ExtractVersions(lessOps, origVersion string, ftype string) (string, string)
 		lastAffected = strings.TrimSpace(validVersion[1])
 	}
 	return introduce, lastAffected
-}
-
-func FindVersion(versionString string) string {
-	versionParts := strings.Split(versionString, " ")
-	if len(versionParts) == 1 {
-		return strings.TrimSpace(versionString)
-	}
-	if len(versionParts) == 2 {
-		for _, p := range versionParts {
-			candidate, err := version.Parse(p)
-			if err != nil {
-				continue
-			}
-			return strings.TrimSpace(candidate.String())
-		}
-	}
-	return versionString
 }
 
 func GetMultiIDs(id string) []string {
